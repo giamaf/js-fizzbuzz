570c69339aaaa74018536117505eee0136e790af
giamaf <gianluca.maffucci@hotmail.com>
2023-12-20 19:02:04 +0100

    js_added FOR cicle

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log('JS OK');
+// Scrivi un programma che stampi in console i numeri da 1 a 100, ma che:
+
+// - per i multipli di 3 stampi “Fizz” al posto del numero
+// - per i multipli di 5 stampi “Buzz”
+// - per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”
+
+// ANALISI 
+
+// 1 - utilizzo FOR per creare un ciclo di stampa da 1 a 100
+// 2 - individuo le condizioni per stabilire i multipli di 3, di 5, di 3 e 5 all'interno del ciclo
+
+
+// VARIABILI FISSE
+const numberThree = 3;
+const numberFive = 5;
+
+
+
+// 1 - utilizzo FOR per creare un ciclo di stampa da 1 a 100
+
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+}
