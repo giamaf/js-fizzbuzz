6d9b1b0c14246ebc9efc6f8e5081935abd5fd97f
giamaf <gianluca.maffucci@hotmail.com>
2023-12-20 19:37:11 +0100

    js_added print multiple condition

--- js/script.js
+++ js/script.js
@@ -4,20 +4,36 @@
 // - per i multipli di 5 stampi “Buzz”
 // - per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”
 
-// ANALISI 
+// ANALISI
 
 // 1 - utilizzo FOR per creare un ciclo di stampa da 1 a 100
 // 2 - individuo le condizioni per stabilire i multipli di 3, di 5, di 3 e 5 all'interno del ciclo
 
-
-// VARIABILI FISSE
-const numberThree = 3;
-const numberFive = 5;
-
-
+// --------------------------------------------------------------------- \\
 
 // 1 - utilizzo FOR per creare un ciclo di stampa da 1 a 100
 
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
+
+    // 2 - individuo le condizioni per stabilire i multipli di 3, di 5, di 3 e 5 all'interno del ciclo
+
+    // Numeri non multipli di 3, 5, 3 e 5
+    if (i % 3 != 0 && i % 5 != 0) {
+        console.log(i);
+    }
+
+    // Multipli di 3 e 5 con FIZZBUZZ
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log('FizzBuzz');
+    }
+
+    // Multipli SOLO di 3 con FIZZ
+    if (i % 3 == 0 && i % 5 != 0) {
+        console.log('Fizz');
+    }
+
+    // Multipli SOLO di 5 con BUZZ
+    if (i % 5 == 0 && i % 3 != 0) {
+        console.log('Buzz');
+    }
 }
